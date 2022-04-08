5c7d4a4cc834fbeff1d490fe987359e5f99fa091
Yuri H. Lopes <88748286+yulopesckp@users.noreply.github.com>
2022-04-07 21:45:48 -0300

    Update FiniteDifferences1and2.py

--- FiniteDifferences1and2.py
+++ FiniteDifferences1and2.py
@@ -2,39 +2,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import linalg
 
+
 def func1(x,y):
     return 10*(x-1)
 
+
+def func2(x,y):
+    return -9.8
+
+
 def first_order(func, y0, yf, x0, b, n):
-    #n =  round((b-x0)/h) + 1
-    h = (b-x0)/(n-1)
+
+    h = (b-x0)/(n)
     y = np.zeros(n)
     y[0] = y0
-    m = np.zeros((n, n))
-    m[0, 0] = 1
+    y[-1] = yf
+    A = np.zeros((n, n))
+    A[0, 0] = 1
     x = np.linspace(x0, b, n)
 
     for i in range(1, n-1):
-        m[i, i+1] = 1
-        m[i, i] = -1
+        A[i, i+1] = 1
+        A[i, i] = -1
         y[i] = h*func(x[i], y[i])
     
-    m[-1, -1] = 1
-    y[-1] = yf
-    f = linalg.solve(m, y)
-    
-    print(f)
-    print(x)
-    #print(y)
-    
+    A[-1, -1] = 1
+    f = linalg.solve(A, y)    
     plt.style.use('dark_background')
     plt.xlabel("x")
     plt.ylabel("y")
     plt.plot(x, f, 'bo--')
     plt.grid()
     plt.show()
-    print(np.dot(m, y) == f)
     return None
 
 
-first_order(func1, 5, 1, 0, 1, 11)
+
+def second_order(func, y0, yf, x0, b, n):
+    
+    h = (b-x0)/(n)
+    y = np.zeros(n)
+    y[0] = y0
+    y[-1] = yf
+    A = np.zeros((n, n))
+    A[0, 0] = 1
+    x = np.linspace(x0, b, n)
+
+    for i in range(1, n-1):
+        A[i, i+1] = 1
+        A[i, i] = -2
+        A[i,i-1] = 1
+        y[i] = (h**2)*func(x[i], y[i])
+    
+    A[-1, -1] = 1
+    f = linalg.solve(A, y)    
+    plt.style.use('dark_background')
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.plot(x, f, 'bo--')
+    plt.grid()
+    plt.show()
+    return None
+
+
+if __name__ == '__main__':
+    first_order(func1, 5, 0, 0, 1, 11)
+    second_order(func2, 0, 50, 0, 5, 10)
