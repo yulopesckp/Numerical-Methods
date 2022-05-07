cbe4be64c2d02d9176589cd75ce776058d0ba1f9
Yuri H. Lopes <88748286+yulopesckp@users.noreply.github.com>
2022-05-07 19:33:15 -0300

    Update Res_L3.py

--- Res_L3.py
+++ Res_L3.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
 
 def M(n):
@@ -31,23 +30,22 @@ def first_order(func, fc, M, y0, yf, x0, b, n):
     x = np.linspace(x0, b, n)
     A = M(n)
     
-    for j in range(10*n):
+    for j in range(5*n):
             
         for i in range(1, n-1):
             B[i] = 2*h*func(x[i],y[i])
             
-        f = np.linalg.solve(A, B)
-        y = f
+        y = np.linalg.solve(A, B)
     
-    f = np.matmul(np.linalg.inv(A), B)
+    y = np.matmul(np.linalg.inv(A), B)
     v = np.linspace(x0, b, 1000)
     h = fc(v)
     plt.figure(dpi = 1200)
-    plt.plot(v, h, 'r', label = "Solução analítica")
     plt.style.use('dark_background')
     plt.xlabel("x")
     plt.ylabel("y")
-    plt.plot(x, f, 'b.--', label = "Método de Diferenças finitas para " + str(n) + " pontos.")
+    plt.plot(x, y, 'b.--', label = "Método de Diferenças finitas para " + str(n) + " pontos.")
+    plt.plot(v, h, 'r', label = "Solução analítica")
     plt.legend(loc = "upper center")
     plt.grid()
     plt.show()
@@ -61,12 +59,9 @@ def main():
     func1 = lambda x, y : y*(x**3-1.5)
     fc = lambda x : np.exp((x**4)/4 - 1.5*x)
     
-    for i in range(1):
+    for i in range(3):
         
-        inicio = time.time()
         first_order(func1, fc, M, 1, 2.72, 0, 2, t1)
-        fim = time.time()
-        print("Para " + str(t1) + " iterações, " + " t = " + str(fim - inicio) + " segundos.")
         t1 *= 10
     
 
